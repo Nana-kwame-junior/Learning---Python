fe16273a2a0dce4f139f78113331d1ae075b637e
GodwinK123 <Kpanazogodwin123@gmail.com>
2022-07-18 11:28:42 -0700

    create my new file

---
+++ python3.py
@@ -0,0 +1,8 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Godwin/AppData/Local/Programs/Python/Python310/python 2.py
+Enter the numbers in the range 1 to 100 :2
+not weird
+3
+3
