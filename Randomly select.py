f4f22e9c609ad32492875335bfd6ecb07a8e6a84
NK <98242562+EnochgithubNK@users.noreply.github.com>
2022-08-21 13:25:16 +0000

    Add files via upload
    Random selection of Names

---
+++ Randomly select.py
@@ -0,0 +1,14 @@
+import random 
+AXRL8_Names=['Asigbey Ezekiel Kofi','Kpotor Seth Mawunya' ,'Atali Edwin Edudzi' 'Atula Sylvia','Kpanazo Godwin',' Atsyatsya Samuel','Agroh Felix','Tsonyake Elikplim','Okyere Prosper','Akoto Ernest','Obeng clinton','Paa Kwesi Bartels'
+    ,'Akakpo Ernest K.M','Akotey Precious','Enoch kwame','Agbozo Theodore','Emmanuel Tokoli','Nabil Ablorh','Tettevia Sonidhi Sefakor','Nubor Kwame Eric','Israel Setugah','Letsu Emmanuel Joe','Sitty Collins','Ekpe Frederick']
+x=random.sample(AXRL8_Names,k=1)
+Excluded=['Enoch kwame','Nubor Kwame Eric','Letsu Emmanuel Joe','Okyere Prosper','Sitty Collins','Ekpe Frederick','Asigbey Ezekiel Kofi']
+Excluded=[]
+while AXRL8_Names !=Excluded:
+    print(x)
+  
+elif():
+    print('Generate a name randmonly{}'.format(AXRL8_Names))
+    
+   
+
