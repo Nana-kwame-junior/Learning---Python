c1ec7fba57e00568bc60d96648fb7b01b78e7e96
NK <98242562+EnochgithubNK@users.noreply.github.com>
2022-08-21 10:24:44 +0000

    Add files via upload
    Dictionary code base on identify

---
+++ Dictionary code.py
@@ -0,0 +1,8 @@
+#Dictionary code:::
+student = {'fname':'Nana','lname':'Junior', 'DOB': '31/05/1997', 'programme':'ICT', 'level':100, 'dept': 'CSC'}
+x={'school':'HTU','Loca':'HO'}
+if (student!=x) and (student != x):
+    print(student["dept"])
+else:
+    print('nothing please is happening')
+print(x['school'])
